1ec978376612e85aa4ba5e7c516e7d1259b85147
(ツ) <guttenbergovitz@gmail.com>
2023-12-29 20:09:10 +0100

    fiddle up with pycharm

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('home.jinja')
+    return render_template('home.j2')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.jinja')
-
+    return render_template('about.j2')
 
 @app.route('/warum')
 def warum():
-    return "Warum? Darum! Warum nicht?"
+    param1 = request.args.get('param1', default = 'default_value1', type = str)
+    param2 = request.args.get('param2', default = 'default_value2', type = str)
+    # Use the parameters in your function as needed
+    return render_template('warum.jinja', param1=param1, param2=param2)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
