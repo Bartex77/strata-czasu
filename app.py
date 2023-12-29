c71bb188eb5415f469468645af9ee95abb86ee4a
(ツ) <guttenbergovitz@gmail.com>
2023-12-29 19:22:20 +0100

    some tweaking as playing with pycharm

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('home.jinja')
 
+
 @app.route('/about')
 def about():
     return render_template('about.jinja')
-  
+
+
 @app.route('/warum')
 def warum():
     return "Warum? Darum! Warum nicht?"
 
 if __name__ == '__main__':
-    app.run(debug=Flask, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
